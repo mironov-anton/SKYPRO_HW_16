5a65a6f9fc54a3fe0da8a48df8a26665f0caff65
mironov-anton <miron.khuzd@gmail.com>
2022-01-09 16:23:32 +0300

    routes with GET method completed for users, orders and offers

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 import raw_data
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JSON_AS_ASCII'] = False
 
@@ -74,7 +74,7 @@ class Offer(db.Model):
         }
 
 
-db.drop_all()
+# db.drop_all() # uncomment when creating db in a file, not in memory
 db.create_all()
 
 for user_data in raw_data.users:
@@ -89,7 +89,6 @@ for user_data in raw_data.users:
     )
     db.session.add(new_user)
     db.session.commit()
-
 
 for order_data in raw_data.orders:
     new_order = Order(
@@ -106,7 +105,6 @@ for order_data in raw_data.orders:
     db.session.add(new_order)
     db.session.commit()
 
-
 for offer_data in raw_data.offers:
     new_offer = Offer(
         id=offer_data["id"],
@@ -116,9 +114,59 @@ for offer_data in raw_data.offers:
     db.session.add(new_offer)
     db.session.commit()
 
-@app.route('/')
-def route():
-    pass
+
+@app.route("/users", methods=['GET', 'POST'])
+def users():
+    if request.method == "GET":
+        results = []
+        for user in User.query.all():
+            results.append(user.to_dict())
+        return jsonify(results), 200
+
+
+@app.route("/users/<int:uid>", methods=['GET'])
+def get_user(uid):
+    user = User.query.get(uid)
+    if user:
+        return jsonify(user.to_dict()), 200
+    else:
+        return f"No user found with id: {uid}", 404
+
+
+@app.route("/orders", methods=['GET', 'POST'])
+def orders():
+    if request.method == "GET":
+        results = []
+        for order in Order.query.all():
+            results.append(order.to_dict())
+        return jsonify(results), 200
+
+
+@app.route("/orders/<int:oid>", methods=['GET'])
+def get_order(oid):
+    order = Order.query.get(oid)
+    if order:
+        return jsonify(order.to_dict()), 200
+    else:
+        return f"No order found with id: {oid}", 404
+
+
+@app.route("/offers", methods=['GET', 'POST'])
+def offers():
+    if request.method == "GET":
+        results = []
+        for offer in Offer.query.all():
+            results.append(offer.to_dict())
+        return jsonify(results), 200
+
+
+@app.route("/offers/<int:ofid>", methods=['GET'])
+def get_offer(ofid):
+    offer = Offer.query.get(ofid)
+    if offer:
+        return jsonify(offer.to_dict()), 200
+    else:
+        return f"No offer found with id: {ofid}", 404
 
 
 if __name__ == "__main__":
