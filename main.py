625873dc043e0903da3785152a61d37675da651d
mironov-anton <miron.khuzd@gmail.com>
2022-01-09 15:44:15 +0300

    database ready, tables filled with data

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import json
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship
+
+import raw_data
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_AS_ASCII'] = False
+
 db = SQLAlchemy(app)
 
 
@@ -18,8 +21,6 @@ class User(db.Model):
     email = db.Column(db.String(100))
     role = db.Column(db.String(100))
     phone = db.Column(db.String(20))
-    orders = relationship("Order")
-    offers = relationship("Offer")
 
     def to_dict(self):
         return {
@@ -38,8 +39,8 @@ class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     description = db.Column(db.String)
-    start_date = db.Column(db.Date)
-    end_date = db.Column(db.Date)
+    start_date = db.Column(db.String)
+    end_date = db.Column(db.String)
     address = db.Column(db.String)
     price = db.Column(db.Integer)
     customer_id = db.Column(db.Integer, db.ForeignKey("user.id"))
@@ -73,8 +74,47 @@ class Offer(db.Model):
         }
 
 
+db.drop_all()
 db.create_all()
 
+for user_data in raw_data.users:
+    new_user = User(
+        id=user_data["id"],
+        first_name=user_data["first_name"],
+        last_name=user_data["last_name"],
+        age=user_data["age"],
+        email=user_data["email"],
+        role=user_data["role"],
+        phone=user_data["phone"]
+    )
+    db.session.add(new_user)
+    db.session.commit()
+
+
+for order_data in raw_data.orders:
+    new_order = Order(
+        id=order_data["id"],
+        name=order_data["name"],
+        description=order_data["description"],
+        start_date=order_data["start_date"],
+        end_date=order_data["end_date"],
+        address=order_data["address"],
+        price=order_data["price"],
+        customer_id=order_data["customer_id"],
+        executor_id=order_data["executor_id"]
+    )
+    db.session.add(new_order)
+    db.session.commit()
+
+
+for offer_data in raw_data.offers:
+    new_offer = Offer(
+        id=offer_data["id"],
+        order_id=offer_data["order_id"],
+        executor_id=offer_data["executor_id"]
+    )
+    db.session.add(new_offer)
+    db.session.commit()
 
 @app.route('/')
 def route():
